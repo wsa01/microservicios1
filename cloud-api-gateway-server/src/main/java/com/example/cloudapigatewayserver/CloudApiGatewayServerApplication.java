95ad51bd026ec48c72d04c76097bd47f01ab78ac
wilder <wilder.salinas.ap@gmail.com>
2023-03-08 00:51:57 -0500

    fix 4

---
+++ cloud-api-gateway-server/src/main/java/com/example/cloudapigatewayserver/CloudApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ cloud-api-gateway-server/src/test/java/com/example/cloudapigatewayserver/CloudApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
